50cd2a2e23dfaeaf0f1835d42c33cc03d4d7d1f1
VinayMavi <vinaymavi@gmail.com>
2017-06-03 15:24:30 +0530

    Initial setup.

---
+++ webapp.py
@@ -0,0 +1,19 @@
+import webapp2
+import os
+from google.appengine.ext.webapp import template
+
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        template_values = {
+            'msg': "Hello world from template :)",
+        }
+
+        path = os.path.join(os.path.dirname(__file__), 'partials/index.html')
+        self.response.out.write(template.render(path, template_values))
+
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage)
+], debug=True)
